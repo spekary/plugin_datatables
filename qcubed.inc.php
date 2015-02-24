316bdd359df4e03cfe55348bf108e407ea25b54c
spekary <spekary@gmail.com>
2015-02-24 13:51:33 -0800

    Regenerating control for new style of controls.

---
+++ qcubed.inc.php
@@ -0,0 +1,7 @@
+<?php
+
+if (!defined ('__PREPEND_INCLUDED__')) {	// not already included some other way (like with .htaccess file)
+	// expect this file to be in the vendor/qcubed/framework directory
+	require(dirname(__FILE__).'/../../framework/qcubed.inc.php');
+}
+?>
